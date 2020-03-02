6643cb412b7f23544bf2cc5e35f9cadc787ed182
han6054 <424681370@qq.com>
2020-03-02 19:51:43 +0800

    purComponent

---
+++ src/doc/9.purComponent.js
@@ -0,0 +1,70 @@
+// import React, { Component, PureComponent} from 'react'
+ import React, { Component} from 'react'
+
+// purComponent 用来优化react，只有state的更改的值变化才触发子组件对应的render函数执行
+
+
+class PureComponent extends Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return !shallowEqual(this.props, nextProps) || !shallowEqual(this.state,nextState);
+    }
+}
+// // 浅比较 obj1和obj2是否相等 只是比较一层
+function shallowEqual(obj1,obj2) {
+   if(obj1 === obj2) {
+       return true;
+   }
+    if(typeof obj1 !='object' || obj1 === null ||typeof obj2 !='object' || obj2 === null){
+        return false;
+    }
+    let keys1 = Object.keys(obj1);
+    let keys2 = Object.keys(obj2);
+    if(keys1.length !== keys2.length) {
+        return  false;
+    }
+    for(let key of keys1) {
+        if(!obj2.hasOwnProperty(key) || obj1[key]!== obj2[key]) {
+            return false;
+        }
+
+    }
+    return true;
+}
+
+export default class App extends PureComponent {
+    constructor(props){
+        super(props);
+        this.state = {title:'计数器',number:0};
+        this.inputRef = React.createRef();
+    }
+    add = ()=>{
+        this.setState({
+            number:this.state.number + parseInt(this.inputRef.current.value) || 0
+        });
+    };
+    render() {
+        console.log('App render');
+        return (
+            <div>
+                <Title title={this.state.title}/>
+                <Counter number={this.state.number}/>
+                <input ref={this.inputRef}/>
+                <button onClick={this.add}>+</button>
+            </div>
+        )
+    }
+}
+
+class Counter extends PureComponent{
+    render(){
+        console.log('Counter render');
+        return <div>{this.props.number}</div>
+    }
+}
+
+class Title extends PureComponent{
+    render(){
+        console.log('Title render');
+        return <div>{this.props.title}</div>
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 // import GetDerivedStateFromProps from './6.getDerivedStateFromProps'
 // import GetSnapshotBeforeUpdate from './7.getSnapshotBeforeUpdate'
-import Context from './8.context'
-ReactDom.render(<Context/>,document.getElementById('root'));
+// import Context from './doc/8.context'
+import PurComponent from './doc/9.purComponent'
+ReactDom.render(<PurComponent/>,document.getElementById('root'));
